2f894155c219438101d467d094b849859f61b842
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-02-02 13:13:46 +0100

    Lab done. Fabrizio and Alexia

--- index.js
+++ index.js
@@ -1,9 +1,118 @@
 //Make sure to require your model so you can connect to the DB
-const { Model } = require("mongoose");
+const VikingModel = require("./models/User.js")
 
 //This line of code connects to the Database
-require("./db");
+// require("./db");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1/valhalla", { family: 4 })
+  .then(() => {
+    console.log(`Connected to Mongo! Welcome to Valhalla!  🪓🛡️☠️`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to DB", err);
+  });
 
 //Iteration 2:
 
 //Iteration 3:
+
+// This is Robert
+const Robert = {
+    firstName: "Robert",
+    familyName: "Edgard",
+    strength: 9,
+    weapon: "Sword",
+}
+
+// Creating Robert inside the collection
+// VikingModel.create(Robert)
+//     .then((oneVik)=>{
+//         console.log(oneVik, "Has been created")
+//     })
+//     .catch((err)=>{
+//         console.log("You ran into an error:", err)
+//     })
+
+// FindByIdAndDelete
+// VikingModel.findByIdAndDelete("63dba1331ea7039ed371f288")
+//     .then((deletedVik)=>{
+//         console.log("You deleted", deletedVik)
+//     })
+//     .catch((err)=>{
+//         console.log("You ran into an error:", err)
+//     })
+
+// FindOneAndDelete
+// VikingModel.findOneAndDelete({firstName: "Robert"})
+    // .then((deletedVik)=>{
+    //     console.log("You deleted", deletedVik)
+    // })
+    // .catch((err)=>{
+    //     console.log("You ran into an error:", err)
+    // })
+
+
+// Viking Array
+// const vikingArr = [
+//     {   
+//         firstName: "Bjørn",
+//         familyName: "Torstenson",
+//         strength: 50,
+//         weapon: "Longbow",
+//     },
+//     {
+//         firstName: "Helga",
+//         familyName: "Edgardson",
+//         strength: 50,
+//         weapon: "Knife",
+//     },
+//     {
+//         firstName: "Leif",
+//         familyName: "Erikson",
+//         strength: 25,
+//         weapon: "Mace",
+//     },
+//     {
+//         firstName: "Frida",
+//         familyName: "Erikson",
+//         strength: 70,
+//         weapon: "Flowers",
+//     }
+// ]
+
+// Insert Many iteration
+// VikingModel.insertMany(vikingArr)
+//     .then((allVikings)=>{
+//         console.log("You created a lot of vikings:", allVikings)
+//     })
+//     .catch((err)=>{
+//         console.log("You ran into an error:", err)
+//     })
+
+// updateOne
+VikingModel.updateOne({familyName: "Erikson"}, {strength: 999}, {new: true})
+    .then((updatedVik)=>{
+        console.log("You updated succesfully", updatedVik)
+    })
+    .catch((err)=>{
+        console.log("You ran into an error:", err)
+    })
+
+// FindOneAndUpdate
+VikingModel.findOneAndUpdate({firstName: "Bjørn"}, {strength: 1000}, {new: true})
+    .then((updatedVik)=>{
+        console.log("You updated succesfully", updatedVik)
+    })
+    .catch((err)=>{
+        console.log("You ran into an error:", err)
+    })
+
+// FindByIdAndUpdate
+VikingModel.findByIdAndUpdate("63dba5613eb3451eda9b82e9", {strength: 130}, {new: true})
+    .then((updatedVik)=>{
+        console.log("You updated succesfully", updatedVik)
+    })
+    .catch((err)=>{
+        console.log("You ran into an error:", err)
+    })
--- models/User.js
+++ models/User.js
@@ -1,5 +1,22 @@
 // require mongoose and destructor Schema and model from it
 const { Schema, model } = require("mongoose");
+
 //Create a viking schema here
+const vikingSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    familyName: {
+        type: String,
+        required: true,
+    },
+    strength: Number,
+    weapon: String,
+});
+
+const VikingModel = model("viking", vikingSchema);
+module.exports = VikingModel
+
 //<======Don't forget Module.export ============>
 //<================Schema=====================>
